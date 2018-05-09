28ee1752b977dcf2c4c76c53b0aba9373ac311b7
Leochens·ZHL <18332518328@163.com>
2018-05-09 12:44:14 +0800

    使用request库

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -1,10 +1,24 @@
 <?php
 namespace app\index\controller;
+use think\Controller;
+use think\Db;
+use think\Request;
 
-class Login
+class Login extends Controller
 {
     public function index()
     {
-    	return '这里是登录界面';
+    	echo '这里是登录界面';
+    	$this->check();
+    	
     }
+    public function check()
+    {
+ 
+    	$req=Request::instance();
+    	//echo $req->url(); 
+    	$data=$req->get();
+    	print_r($data);
+
+	}
 }
