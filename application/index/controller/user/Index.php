27c6782e1b57d20ba245624b1f07639badb5a747
Leochens·ZHL <18332518328@163.com>
2018-05-10 13:12:44 +0800

    使用session完成了用户登录注销等功能 有个基本的页面

--- application/index/controller/user/Index.php
+++ application/index/controller/user/Index.php
@@ -12,23 +12,17 @@ class Index extends Controller
 	}
     public function index()
     {
-    	//echo '这里是首页';
-    	$this->login();
+    	
     	if($this->check())
-    	{
     		$check = "用户".Session::get('name')."已登录";
-    	}else
-    	{
-    		$check = "用户未登陆";
-    	}
+        else
+            $this->error('用户未登陆,请登陆一下.',"index/user.login/index",'',$wait=1);
+
     	$this->assign([
     		'msg'=>'这里是首页',
             'check'=>$check
     		]);
     	return $this->fetch();
-    	/*记得把index这个模块名写上*/
-
-    	//print_r($this->e->deleteByUser(1,3));
     	
     }
 
@@ -39,47 +33,11 @@ class Index extends Controller
     		return 0;
     }
 
-    /**
-     * 登录
-     * @return [type] [description]
-     */
     
-    private function login()
-    {
-    	$req=Request::instance();
-    	$data=$req->param();
-    	if($this->isAdmin($data))
-    	{
-    		//echo "<br>用户".$data['name']."登录成功";
-    		//$this->success("<br>用户".$data['name']."登录成功");
-    		Session::set('user',$data['name']);
-    		return 1;
-    	}else{
-    		//$this->error("<br>用户登录失败，用户名或密码错误");
-    		return 0;
-    	}
-	}
-	/**
-	 * 判断数据库内是否有这个管理员
-	 * @param  [type]  $e [获得的参数]
-	 * @return boolean    [1 是管理员 0 未找到管理员]
-	 */
-	private function isAdmin($e){
-		$res = Db::table('users')
-			->where('name',$e['name'])
-			->where('password',$e['password'])
-			->find();
-		//print_r($res);
-		if($res)
-			return 1;
-		else
-			return 0;
-	}
-
-
 
 	public function logout(){
 		Session::delete('user');
-		return '删除session成功';
-	}
+		$this->success('删除session成功','index/user/login');    //
+        
+        }
 }
--- application/index/controller/user/Login.php
+++ application/index/controller/user/Login.php
@@ -1,8 +1,69 @@
 <?php
-
+namespace app\index\controller\user;
+use think\Controller;
+use think\Db; 
+use think\Request;
+use think\Session;
 /**
  * @Author: Administrator
  * @Date:   2018-05-10 08:59:08
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-05-10 09:12:50
+ * @Last Modified time: 2018-05-10 13:11:35
  */
+
+//登陆程序
+class Login extends Controller{
+
+	private $e;
+	public function _initialize(){
+    	$this->e = controller('index/record/Record');
+	}
+	public function index(){
+		//$this->login();
+		
+		return $this->fetch();
+	}
+
+
+
+    /**
+     * 登录
+     * @return [type] [description]
+     */
+    
+    public function login()
+    {
+    	$req=Request::instance();
+    	$data=$req->param();
+    	if($this->isUser($data))
+    	{
+    		//echo "<br>用户".$data['name']."登录成功";
+    	
+    		Session::set('user',$data['name']);
+    		$this->success("<br>用户".$data['name']."登录成功",'user/index');
+    		return 1;
+    	}else{
+    		$this->error("<br>用户登录失败，用户名或密码错误");
+    		return 0;
+    	}
+	}
+
+	/**
+	 * 判断数据库内是否有这个管理员
+	 * @param  [type]  $e [获得的参数]
+	 * @return boolean    [1 是管理员 0 未找到管理员]
+	 */
+	private function isUser($e){
+		$res = Db::table('users')
+			->where('name',$e['name'])
+			->where('password',$e['password'])
+			->find();
+		//print_r($res);
+		if($res)
+			return 1;
+		else
+			return 0;
+	}
+
+
+}
