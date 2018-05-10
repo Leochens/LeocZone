f7537d439c3300ca0e138cb75362cbe37dd4f95c
Leochens·ZHL <18332518328@163.com>
2018-05-10 09:07:59 +0800

    完善记录类

--- application/index/controller/record/Record.php
+++ application/index/controller/record/Record.php
@@ -6,12 +6,19 @@ use think\Db;
  * @Author: Administrator
  * @Date:   2018-05-09 21:46:03
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-05-09 23:24:25
- * 分册控制器  主要提供其他控制器数据集
+ * @Last Modified time: 2018-05-10 08:56:09
+ * 分层控制器  主要提供其他控制器数据集
  */
 
 class Record extends Controller{
 
+
+	private $table ;
+
+	public function _initialize()
+	{
+		$this->table = Db::table('records');
+	}
 	public function index(){
 
 	}
@@ -21,19 +28,69 @@ class Record extends Controller{
 	 */
 	public function getAll()
 	{
-		$res = Db::table('records')->select();
+		$res = $this->table->select();
 		return $res?$res:-1;
 	}
 	/*
-	获取对应用户的记录数据
+		获取对应用户的记录数据
 	 */
 	public function getByUser($user_id)
 	{
-		$res = Db::table('records')
+		$res = $this->table
 				->where('user_id',$user_id)
+				->where('type',1)
 				->select();
 		return $res?$res:-1;
 	}
+
+	/**
+	 * 根据用户插入记录
+	 * @param [type] $user_id [description]
+	 * @param [type] $data    [description]
+	 */
+	public function InsertByUser($user_id,$data)
+	{
+		$data['user_id']=$user_id;
+		$flag = $this->table
+				->insert($data);
+
+		return $flag?1:0;
+	}
+	public function InsertByAdmin($user_id,$data)
+	{
+		$data['user_id']=$user_id;
+		$data['type']=2;
+		$flag = $this->table
+				->insert($data);
+	}
+	/**
+	 * 根据用户删除记录
+	 * @param [type] $user_id   [description]
+	 * @param [type] $record_id [description]
+	 */
+	public function DeleteByUser($user_id,$record_id)
+	{
+		$flag = $this->table
+			->where('user_id',$user_id)
+			->delete($record_id);
+		return $flag?1:0;
+	}
+	
+	/**
+	 * 根据用户更新记录
+	 * @param [type] $user_id   [description]
+	 * @param [type] $record_id [description]
+	 */
+	public function UpdateByUser($record_id,$data)
+	{
+		//$data['user_id']=$user_id;
+		$flag = $this->table
+				->where('id',$record_id)
+				->update($data);
+		return $flag?1:0;
+
+	}
+	
 
 
 } 
