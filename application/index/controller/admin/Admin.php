fb9005ee93e7e8cee66d80fdf5028cb7869e262e
Leochens·ZHL <18332518328@163.com>
2018-05-11 16:07:08 +0800

    管理员登录逻辑完成

--- application/index/controller/admin/Admin.php
+++ application/index/controller/admin/Admin.php
@@ -3,41 +3,34 @@ namespace app\index\controller\admin;
 use think\Controller;
 use think\Db;
 use think\Request;
-
+use think\Session;
 class Admin extends Controller
 {
     public function index()
     {
-    	echo '这里是登录界面';
-    	$this->check();
+    	if($this->check())
+    	{
+    		$msg = '管理员'.Session::get('admin').'登录成功';
+    	}else{
+    		 $this->error('管理员未登陆,跳转至登陆页面',"index/admin.Login/index");
+    	}
+    	$this->assign([
+    			'msg'=>$msg
+    			]);
+    	return $this->fetch();
     }
     private function check()
     {
-    	$req=Request::instance();
-    	$data=$req->param();
-    	if($this->isAdmin($data))
-    	{
-    		echo "<br>登录成功";
-    	}else{
-    		echo "<br>登录失败，用户名或密码错误";
-    	}
+    	if(Session::has('admin'))
+    		return 1;
+    	else 
+    		return 0;
 	}
+	public function logout(){
+		Session::delete('user');
+		$this->success('删除session成功','admin/Login');    //
+        
+        }
 
-	/**
-	 * 判断数据库内是否有这个管理员
-	 * @param  [type]  $e [获得的参数]
-	 * @return boolean    [1 是管理员 0 未找到管理员]
-	 */
-	private function isAdmin($e){
-		$res = Db::table('admins')
-			->where('name',$e['name'])
-			->where('password',$e['password'])
-			->find();
-		//print_r($res);
-		if($res)
-			return 1;
-		else
-			return 0;
-	}
 
 }
